374edddecd94191bbb217091ea489420f2ac7462
nagraj-packt <35103791+nagraj-packt@users.noreply.github.com>
2019-03-30 12:00:35 +0530

    Codes files added

---
+++ Section 2/2.2 Start/enrollment/application/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return "<h1>Hello Earth!!</h1>"
---
+++ Section 2/2.4 Start/enrollment/application/routes.py
@@ -0,0 +1,7 @@
+from application import app
+from flask import render_template
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html")
